02fb6f3e058e7f85a49db281e88ba153c82161c3
gs195 <gsharma.nus@gmail.com>
2020-02-25 22:46:19 +0800

    ✨ Add mock home page and uploadArena

---
+++ src/app/infoStorage/components/UploadArena.js
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import "semantic-ui-css/semantic.min.css";
+import { Segment, Icon, Button, Header } from "semantic-ui-react";
+
+const UploadArena = () => {
+  return (
+    <Segment placeholder>
+      <Header icon>
+        <Icon name="pdf file outline" />
+        Upload files by dropping them here.
+      </Header>
+      <Button primary>Add Files</Button>
+    </Segment>
+  );
+};
+
+export default UploadArena;
