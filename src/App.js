1f67135377f9e9186ea7346c4d77ddab2513c769
gs195 <gsharma.nus@gmail.com>
2020-12-21 13:15:59 +0800

    🧹Disable note taker

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TabBar from "@common/TabBar";
 const pages = [
   { heading: "Home", path: "/", icon: "home" },
   { heading: "Info Manager", path: "/infoStorage", icon: "clipboard" },
-  { heading: "Study", path: "/noteTaker", icon: "pencil alternate" },
+  // { heading: "Study", path: "/noteTaker", icon: "pencil alternate" },
 ];
 class App extends React.Component {
   constructor(props) {
@@ -21,7 +21,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={HomePageContainer} />
             <Route exact path="/infoStorage" component={InfoStorageContainer} />
-            <Route exact path="/noteTaker" component={NoteTaker} />
+            {/* <Route exact path="/noteTaker" component={NoteTaker} /> */}
           </Switch>
         </BrowserRouter>
       </div>
