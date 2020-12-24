cbf0ef1c51a1cb301643eefbc79c374aef77a82d
gs195 <gsharma.nus@gmail.com>
2020-12-24 10:01:21 +0800

    🎨Add navigate bar transition

--- src/app/navigate/Navigate.js
+++ src/app/navigate/Navigate.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { withRouter } from "react-router-dom";
 
 import { Menu, Icon } from "semantic-ui-react";
@@ -6,11 +6,19 @@ import { Menu, Icon } from "semantic-ui-react";
 const Navigate = ({ history, location }) => {
 	const [dim, setDim] = useState(true);
 
-	const goToPage = (path) => history.push(path);
+	const goToPage = useCallback((path) => history.push(path), [history]);
 
 	return (
 		<div
-			css={{ opacity: dim ? 0.4 : 1, position: "absolute", top: 20, right: 20 }}
+			css={{
+				opacity: dim ? 0.4 : 1,
+				position: "absolute",
+				top: 0,
+				right: 0,
+				padding: 20,
+				transition: "opacity 0.2s ease-in-out",
+				zIndex: 3,
+			}}
 			onMouseEnter={() => setDim(false)}
 			onMouseLeave={() => setDim(true)}>
 			<Menu compact icon="labeled" size="tiny">
@@ -21,7 +29,6 @@ const Navigate = ({ history, location }) => {
 					<Icon name="home" />
 					Home
 				</Menu.Item>
-
 				<Menu.Item
 					name="sticky note"
 					active={location.pathname === "/notes"}
